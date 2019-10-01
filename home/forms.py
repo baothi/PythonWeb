d82c74d519f5e2c88a5c482edbf5eb397ea99806
baothi <baothi246@gmail.com>
2019-10-01 08:37:08 -0700

    fix error

--- home/forms.py
+++ home/forms.py
@@ -1,16 +1,16 @@
 from django import forms
 import re
 from django.contrib.auth.models import User
-from django.core.exceptions import ObjectDoesnotExist
+from django.core.exceptions import ObjectDoesNotExist
 
 class RegistrationForm(forms.Form):
 	username = forms.CharField(lable='Tài Khoản', max_length=30)
 	email = forms.EmailField(lable='Email')
 	password1 = forms.CharField(lable='Mật Khẩu', widget=forms.passwordInput())
-	password2 = forms.CharField(lable='Nhập Lại Mật Khẩu', widget=forms.passwordInput)
+	password2 = forms.CharField(lable='Nhập Lại Mật Khẩu', widget=forms.passwordInput())
 
 	def clean_password2(self):
-		if 'password1' in self.leaned_data:
+		if 'password1' in self.cleaned_data:
 			password1 = self.cleaned_data['password1']
 			password2 = self.cleaned_data['password2']
 			if password1==password2 and password1:
@@ -18,12 +18,12 @@ class RegistrationForm(forms.Form):
 		raise forms.validationError("Mật Khẩu không Hợp Lệ")
 	def clean_username(self):
 		username = self.cleaned_data['username']
-		if re.search(r'^\w+$', username):
+		if not re.search(r'^\w+$', username):
 			raise forms.validationError("ten tai khoan co ky tu dac biet")
 		try:
 			User.objects.get(username=username)
 
-		except ObjectDoesnotExist:
+		except ObjectDoesNotExist:
 			return username
 		raise forms.validationError("tai khoan da ton tai")
 	def save(self):
--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns =[
-path('',views.index),
-path('contact/', views.contact, name='contact'),
-path('register/', views.register, name='register'),
+    path('',views.index),
+    path('contact/', views.contact, name='contact'),
+    path('register/', views.register, name='register'),
 ]
